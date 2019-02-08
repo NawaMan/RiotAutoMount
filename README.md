0406016d5c86edbd3cb865d75e8e09b0f91ae4cc
Nawa Manusitthipol <nawa@nawaman.net>
2019-02-08 14:37:41 -0500

    Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # RiotAutoMount
-RiotAutoMount
+RiotAutoMount listen to element insert event and check if the inserted element is a riot element.
+If it is, `riot.mount(...)` will be run on it.
+
+Simply import the script.
+
+```html
+    <script src="riot-automount.min.js"></script>
+```
+
+## NPM
+    npm install riotautomount
+
+## CDN
+### Non-Minified
+https://cdn.rawgit.com/NawaMan/RiotAutoMount/master/riotautomount.js
+### Minified
+https://cdn.rawgit.com/NawaMan/RiotAutoMount/master/riotautomount.min.js
+
+## Dependency
+This library only require `riot`.
+
+[npm-url]: https://npmjs.org/package/riotautomount
+[downloads-image]: http://img.shields.io/npm/dm/riotautomount.svg
+[npm-image]: http://img.shields.io/npm/v/riotautomount.svg
---
+++ riot-automount.js
@@ -0,0 +1,41 @@
+(function() {
+    /** 
+     * This code is to automatically mount riot element.
+     */
+    document.addEventListener("DOMContentLoaded", function() {
+        document.body.addEventListener('DOMNodeInserted',function(e) {
+            var node = e.target
+            if (node.nodeType != 1) // Not an element
+                return
+            if (node._tag)  // Already mount
+                return
+            
+            if (!(node instanceof HTMLUnknownElement) &&
+                !node.getAttribute('data-is') &&
+                !(node.tagName && node.tagName.match(/.+-.+/)))
+                return
+            
+            var parent = node
+            while (parent != document.body) {
+                if (parent._tag) {
+                    setTimeout(function() {
+                        // Force mount if not yet mount after a while.
+                        // This is the case for when the parent is already added long before the node is added.
+                        if (!node._tag)
+                            riot.mount(node)
+                    }, 100)
+                    return
+                }
+                parent = parent.parentNode
+                if (!parent)
+                    return
+            }
+
+            try { riot.mount(node) }
+            catch(e) {
+                console.error('Fail to mount: Not valid riot tag.');
+            }
+        }, false)
+        riot.mount('*')
+    });
+})()
